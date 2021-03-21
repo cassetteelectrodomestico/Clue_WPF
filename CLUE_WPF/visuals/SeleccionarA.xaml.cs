5a48d77a4398787c1e211eb16d5ada28f22d2f7f
MaritzaHC <33165928+MaritzaHC@users.noreply.github.com>
2021-03-20 19:23:36 -0600

    manualVista

---
+++ CLUE_WPF/visuals/SeleccionarA.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clue_WPF.visuals {
+    /// <summary>
+    /// Lógica de interacción para TextInferior.xaml
+    /// </summary>
+    public partial class SeleccionarA : UserControl {
+
+        public delegate void buttonMethod();
+
+        public buttonMethod clickMethod;
+        public SeleccionarA() {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            clickMethod?.Invoke();
+        }
+    }
+}
